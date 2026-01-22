6e50a1769e5d223daa28dfc3ac6552637b54a32f
Blod <erkhanpiriyev@gmail.com>
2026-01-22 19:07:01 +0500

    Token

--- scripts/Token.sol
+++ scripts/Token.sol
@@ -4,10 +4,19 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20 , Ownable
+contract ResearchToken is ERC20 , Ownable
 {
-	constructor() ERC20("Token" , "RRT") Ownable(msg.sender)
+	constructor() ERC20("ResearchToken" , "RT") Ownable(msg.sender)
 	{
+	}
 
+	function mint(address to , uint256 amount) external onlyOwner
+	{
+		_mint(to , amount);
+	}
+
+	function burn(address from , uint256 amount) external onlyOwner
+	{
+		_burn(from , amount);
 	}
 }
